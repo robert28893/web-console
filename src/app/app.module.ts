0f6e1c936ff0dd550ae1352f407b0af067e2a89e
robert <hoangphuongdtvt288@gmail.com>
2023-06-30 17:56:12 +0700

    update code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,6 +35,7 @@ import { CompetitionComponent } from './component/competition/competition/compet
     FormsModule,
     NgbModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/service/competition/competition.service.ts
+++ src/app/service/competition/competition.service.ts
@@ -1,6 +1,9 @@
 import {Injectable} from '@angular/core';
 import {CompetitionModel} from "../../model/competition/competition.model";
-import {Observable} from "rxjs";
+import {catchError, map, Observable} from "rxjs";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {Constants} from "../../common/constants";
 
 @Injectable({
   providedIn: 'root'
@@ -15,15 +18,47 @@ export class CompetitionService {
     )
   ];
 
-  constructor() {
+  constructor(
+    private httpClient: HttpClient,
+  ) {
   }
 
   public getCompetitions(): Observable<CompetitionModel[]> {
     // TODO: call api
-    return new Observable(subscriber => subscriber.next(this.competitions));
+    // return this.httpClient.get(environment.apiUrl + "/soccer-service/competitions")
+    return this.httpClient.get<any>(environment.apiUrl + "/soccer-service/competitions",
+      this.getHeaders())
+      .pipe(
+        map(value => {
+          console.log(value)
+          console.log(value.object.data)
+          let competitions: CompetitionModel[] = []
+          for(let obj of value.object.data) {
+            competitions.push(new CompetitionModel(
+              obj.id, obj.name, obj.gender, obj.countryName))
+          }
+          return competitions;
+          // return [new CompetitionModel(
+          //   1, 'Premier League', 'male', 'england')]
+        }),
+        catchError(err => {
+          console.log(err)
+          return []
+        })
+      );
   }
 
   public getCompetition(id: number): Observable<CompetitionModel> {
     return new Observable<CompetitionModel>(subscriber => subscriber.next(this.competitions[id - 1]))
+  }
+
+  getHeaders() {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Content-Type', 'application/json')
+    if (localStorage.getItem(Constants.ACCESS_TOKEN)) {
+      // headers = headers.append('Authorization', 'Bearer ' + localStorage.getItem(Constants.ACCESS_TOKEN))
+    }
+
+    return { headers }
   }
 }
